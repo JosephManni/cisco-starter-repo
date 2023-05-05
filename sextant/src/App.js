831a6eb57138171f300a084df69936b93b8ac3c1
Joseph Manni <dev.jmanni@gmail.com>
2023-05-05 03:27:43 -0400

    updated latency

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,14 +3,24 @@ import React, { Component } from "react";
 import Banner from "./Banner";
 import Exhibit from "./Exhibit";
 import IPComponent from './IP';
+import PylonConnection from './PlyonCon';
 
 function App() {
   return (
     <div className="App">
       <Banner bannerContent="Sextant - Dashboard"/>
-      <Exhibit exhibitTitle="Exhibit A" latency="10" url='https://api.ipify.org?format=json'></Exhibit>
-      <Exhibit exhibitTitle="Exhibit B" latency="150" url='https://api64.ipify.org?format=json'></Exhibit>
-      <Exhibit exhibitTitle="Exhibit C" latency="250" url='https://api64.ipify.org?format=json'></Exhibit>
+      <Exhibit exhibitTitle="Exhibit A" 
+              latency="10" 
+              url='https://api.ipify.org?format=json'/>
+      <Exhibit exhibitTitle="Exhibit B" 
+              latency="150" 
+              url='https://api64.ipify.org?format=json'/>
+      <Exhibit exhibitTitle="Exhibit C" 
+              latency="60" 
+              url='https://api.ipify.org?format=json'/>
+      <Exhibit exhibitTitle="Exhibit D" 
+              latency="250" 
+              url='https://api64.ipify.org?format=json'/>
       
     </div>
   );
--- sextant/src/Exhibit.js
+++ sextant/src/Exhibit.js
@@ -2,16 +2,18 @@ import './exhibit.css';
 import React, { Component } from "react";
 import LineChart from './components/LineChart';
 import IPComponent from './IP';
+import PylonCon from './PlyonCon';
 
 class Exhibit extends Component {
   render() {
-    const { exhibitTitle, latency, url } = this.props;
+    const { exhibitTitle, url } = this.props;
+    var latency = 0;
     const color = latency < 99 ? 'green' : latency < 199 ? 'yellow' : 'red'; // Change the thresholds and colors as needed
     return (
       <div className="exhibit">
         <div className="exhibit-header">
           <h3>{exhibitTitle}</h3>
-          <p style={{ color }}>Latency: {latency} (m/s)</p>
+          <p style={{ color }}>Latency: <PylonCon></PylonCon> (m/s)</p>
         </div>
         <LineChart></LineChart>
         <IPComponent url={url}></IPComponent>
